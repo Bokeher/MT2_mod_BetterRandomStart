048c17163f160f415d88b4ce7d1ef76f8aae25d0
Bokeher <bokeher47@gmail.com>
2025-06-15 19:53:59 +0200

    Add clan ID-to-name mapping and champion index array

--- MT2_BETTER_RANDOM/BetterRandom.cs
+++ MT2_BETTER_RANDOM/BetterRandom.cs
@@ -8,6 +8,22 @@ namespace MT2_BETTER_RANDOM
     [BepInPlugin("com.bokeher.better_random", "Better Random", "1.0.0")]
     public class BetterRandom : BaseUnityPlugin
     {
+        private readonly Dictionary<string, string> clans = new Dictionary<string, string>
+        {
+            { "5be08e27-c1e6-4b9d-b506-e4781e111dc8", "Banished" },
+            { "ab9c9f6f-2543-4ca5-b7e5-e2eb125445b8", "Pyreborne" },
+            { "0df83271-5359-48df-9365-e73b7b2d0130", "Luna Coven" },
+            { "3c98c8eb-fc7c-4b35-925e-6b5ab0f69896", "Underlegion" },
+            { "9aaf1009-3fbe-4ac5-9f99-a3a702ff7f27", "Lazarus League" },
+            { "c595c344-d323-4cf1-9ad6-41edc2aebbd0", "Hellhorned" },
+            { "fd119fcf-c2cf-469e-8a5a-e9b0f265560d", "Awoken" },
+            { "9317cf9a-04ec-49da-be29-0e4ed61eb8ba", "Stygian Guard" },
+            { "4fe56363-b1d9-46b7-9a09-bd2df1a5329f", "Umbra" },
+            { "fda62ada-520e-42f3-aa88-e4a78549c4a2", "Melting Remnant" },
+        };
+
+        private readonly int[] championIndexes = { 0, 1 };
+
         private void Update()
         {
             // Find an object of type CompendiumSection
